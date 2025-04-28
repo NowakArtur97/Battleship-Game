2376474277550f1cd8333cc89bb58c357c78036b
NowakArtur97 <nowart97@gmail.com>
2025-04-28 15:49:59 +0200

    Generate project using Spring Initializr

---
+++ server/src/main/java/com/nowakartur97/battleshipgame/BattleshipgameApplication.java
@@ -0,0 +1,13 @@
+package com.nowakartur97.battleshipgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipgameApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/nowakartur97/battleshipgame/BattleshipgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakartur97.battleshipgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
