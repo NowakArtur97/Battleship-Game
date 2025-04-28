32d747e7d20ac530447afa4785266c5c084a4a5d
NowakArtur97 <nowart97@gmail.com>
2025-04-28 16:24:47 +0200

    Create configuration for websockets

--- server/src/main/java/com/nowakartur97/battleshipgame/game/config/WebSocketConfig.java
+++
@@ -1,9 +0,0 @@
-package com.nowakartur97.battleshipgame.game.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebSocketConfig extends AbstractWebSocketMessageBrokenConfigurer {
-
-
-}
