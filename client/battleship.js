7ddb31ae6921e459ea6b86bbec4bb0288e7f05db
NowakArtur97 <nowart97@gmail.com>
2025-06-17 12:29:04 +0200

    Display and animate ship sprite

--- client/battleship.js
+++ client/battleship.js
@@ -713,6 +713,9 @@ document.addEventListener("DOMContentLoaded", () => {
           square.classList.add(
             ...["board__square", `board__square--${squareType.description}`]
           );
+          const shipAnimation = document.createElement("div");
+          shipAnimation.classList.add("board__square_image");
+          square.appendChild(shipAnimation);
           const isInNotInFirstRowAndColumn = row !== 0 && column !== 0;
           if (isInNotInFirstRowAndColumn) {
             const position = new Position(column, row);
